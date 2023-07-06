a649df7b03af594d52226495fe3b206b87c4ed97
jsSteud <95288822+jsSteud@users.noreply.github.com>
2023-07-06 10:48:25 +0200

    dbug

--- src/main.js
+++ src/main.js
@@ -4,4 +4,6 @@ import router from './router'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
+document.title = 'Welcome to Gymplan!'
+
 createApp(App).use(router).mount('#app')
